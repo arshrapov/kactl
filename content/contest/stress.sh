66414453d709590b02d27667c4151f72756588cd
raf <rafael@maximum.software>
2024-11-15 15:15:46 +0500

    minor changes

---
+++ content/contest/stress.sh
@@ -0,0 +1,11 @@
+g++ -std=c++17 -O2 smart.cpp -o smart
+g++ -std=c++17 -O2 stupid.cpp -o stupid
+g++ -std=c++17 -O2 gen.cpp -o gen
+
+for t in $(seq 1 100000); do
+    echo "Running test $t"
+    ./gen $t > input
+    ./smart < input > smart_output
+    ./stupid < input > stupid_output
+    diff smart_output stupid_output || exit
+done
